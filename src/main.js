d79fe4f541dcd8a36951aaae4d2d1bb437a946ad
caiocroccia22 <caiocroccia22@gmail.com>
2025-02-12 22:09:11 -0300

    feat: implement header section and event to hide button and brand

--- src/main.js
+++ src/main.js
@@ -1,7 +1,38 @@
 document.addEventListener('DOMContentLoaded', function(){
+    // Seleciona os botões que possuem o atributo
     const buttons = document.querySelectorAll('[data-tab-button]');
+
+    // 
     const questions = document.querySelectorAll('[data-faq-question]');
+
+    // Seleciona a section Hero
+    const heroSection = document.querySelector('.hero');
+//-------------------------------------------------------------------------
+    // Section Herro
+
     
+    // Pega a altura do Hero, para saber quando o mouse passou por ele
+    const heroHeight = heroSection.clientHeight;
+
+    // Evento que chama a função que localiza a posição do scrool
+    window.addEventListener('scroll', function(){
+
+        // Set a posição do scroll na variável position
+       const position = window.scrollY;
+
+    //   Condicional para quando a posição do mouse for menor que o heroHeight
+        if (position < heroHeight){
+            // Chama a função
+            hiddenHeaderElements()
+        }
+        else {
+            showHeaderElements()
+        }
+        
+    })
+    
+    //  Section Atrações
+
     ///Para trocar o layout quando clicar nas abas
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
@@ -19,6 +50,7 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     }
 
+    // Section FAQ
     for (let i = 0; i<questions.length ; i++){
         questions[i].addEventListener("click", function(e){
             const classe = 'faq__questions__item--is-open';
@@ -27,9 +59,23 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     }
 
-
-
 })
+//------------------------------------------------------------------------------
+
+
+//Function para adicionar a Classe 'Header--is--hidden'
+function hiddenHeaderElements(){
+    // Seleciona o Elemento Header
+    const header = document.querySelector('header');
+    // Adiciona a Classe 
+    header.classList.add('header--is-hidden');
+}
+
+//Function para retirar a Classe 'Header--is--hidden'
+function showHeaderElements(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function escondeTodasAbas(){
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
